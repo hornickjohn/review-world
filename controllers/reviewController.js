6b6e6e3b7c54c87fc1045cb7e29ec2c415270d23
byaylin <aylinxmorales@gmail.com>
2023-02-10 14:12:36 -0800

    review contoller

---
+++ controllers/reviewController.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const{ User, Category, Review} = require ('../models');
+
+router.get('/', (req, res) =>{
+    Review.findAll().then(data =>{
+        res.json(data)
+    }).catch(err=>{
+        console.log(err)
+    })
+});
+
+router.get('/review', (req, res)=>{
+    User.findByPk(req.params.review,{
+        include:[Category]
+    }).then((data)=>{
+        res.status(200).json(data)
+    }).catch(err=>{
+        console.log(err)
+    })
+});
+
+router.post('/',(req,res)=>{
+    Review.create(req.body).then((data)=>{
+        return res.status(200).json(data);
+    }).catch(err=>{
+        console.log(err)
+    })
+});
+
+router.put('/review',(req,res)=>{
+    Review.update({
+        review_text:req.body.review_text
+    },{
+        where: {
+            review:req.params.review
+        }
+    }).then((data)=>{
+        res.status(200).json(data)
+    }).catch(err=>{
+        console.log(err)
+    })
+})
+
+router.delete('/review', (req,res)=>{
+    Review.destroy({
+        where: {
+            review:req.params.review
+        }
+    }).then((data)=>{
+        res.status(200).json(data)
+    }).catch(err=>{
+        console.log(err)
+    })
+})
+
+module.export = router;
