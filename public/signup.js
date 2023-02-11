312a673aa14657c32595e247d36df14fd7503c81
Zazil Gomz <zazilgomez@Zazils-MacBook-Air.local>
2023-02-10 18:28:12 -0800

    public created

---
+++ public/signup.js
@@ -0,0 +1,22 @@
+document.querySelector("#signupForm").addEventListener("submit",e=>{
+    e.preventDefault();
+    const signupObj = {
+        email:document.querySelector("#signupEmail").value,
+        password:document.querySelector("#signupPassword").value,
+        username:document.querySelector("#signupUsername").value
+    }
+    console.log(signupObj)
+    fetch("/api/users",{
+        method:"POST",
+        body:JSON.stringify(signupObj),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        if(res.ok){
+           location.href="/profile"
+        } else {
+            alert("signup failed")
+        }
+    })
+})
