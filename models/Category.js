d9769235d9548cb6274de3ad11ad2c4360b57aa0
Zazil Gomz <zazilgomez@Zazils-MacBook-Air.local>
2023-02-11 19:39:59 -0800

    add seeds

--- models/Category.js
+++ models/Category.js
@@ -4,6 +4,12 @@ const sequelize = require('../config/connection');
 class Category extends Model {}
 
 Category.init({
+    id: {
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        primaryKey:true,
+        autoIncrement:true
+      },
     // add properites here, ex:
     name: {
          type: DataTypes.STRING,
@@ -13,7 +19,10 @@ Category.init({
          }
     }
 },{
-    sequelize
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "category",
 });
 
 module.exports=Category
